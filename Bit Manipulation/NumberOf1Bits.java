26aac6183e2bc58f17485bf1ab431bfe9d193fca
Yash Upase <yashupase777@gmail.com>
2026-05-10 23:51:08 +0530

    Add function comment for hammingWeight
    Add a comment explaining the hammingWeight function and its purpose.

--- Bit Manipulation/NumberOf1Bits.java
+++ Bit Manipulation/NumberOf1Bits.java
@@ -1,3 +1,16 @@
+/*Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+
+ 
+
+Example 1:
+
+Input: n = 11
+
+Output: 3
+
+Explanation:
+
+The input binary string 1011 has a total of three set bits.*/
 class Solution {
     public int hammingWeight(int n) {
 
